decd03075ccae1acf9b7db27abd9109887ef146e
Nouval Rizky <nouvalr@gmail.com>
2022-04-13 15:23:23 +0800

    Add validation for Food, invalid with price less than 0.01

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,8 @@ class Food < ApplicationRecord
     validates :description, presence: true
 
     validates :name, presence: true, uniqueness: true
+
+    validates :price, numericality: true, comparison: { greater_than_or_equal_to: 0.01 }
   
     def self.by_letter(letter)
       where("name LIKE ?", "#{letter}%").order(:name)
